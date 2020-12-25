314062c350010cba8a25d13b70ea02a602348f33
Adam Kalimi <adam.kalimi@yahoo.com>
2020-12-25 15:41:15 +0800

    Added Block.vue and Results.vue. Included functionality for the game to start - the delay time is determined, the status of the game 'isPlaying' becomes true. Data binding on the Block component within App.vue passes the delay time to the Block.vue component. Results.vue has not been developed yet.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>How fast is your reaction time?</h1>
+    <!-- disabled will disable the btn. It checks the binded data for the condition. -->
+    <button @click="start" :disabled="isPlaying">Play</button>
+    
+      <!-- v-bind: || : - both enable us to bind data -->
+      <Block v-if="isPlaying" :delay="delay"/>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
   }
 }
 </script>
@@ -20,7 +38,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
